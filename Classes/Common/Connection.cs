bb86b217d3a9aa3d7a9a2b202c33d5649f967232
mynirka1234 <mahmutovamunira0@gmail.com>
2024-06-03 15:41:01 +0500

    Создан класс подключения к бд

---
+++ Classes/Common/Connection.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uchot.Classes.Common
+{
+    public class Connection
+    {
+        public static readonly string config = "server=localhost;port=3306;database=uchot_oborud;uid=root;pwd=;";
+        public static MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+        public static MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            return new MySqlCommand(SQL, connection).ExecuteReader();
+        }
+        public static void ConnectionClose(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+
+        }
+    }
+}
